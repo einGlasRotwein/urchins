e3f22b776e5b7ae4728ea351e52550e72f23cf06
einGlasRotwein <juliane.tkotz@gmail.com>
2020-07-22 17:38:04 +0200

    exploring feedback sum

--- first_look.R
+++ first_look.R
@@ -19,3 +19,11 @@ data %>%
   ggplot(aes(x = feedback_type_rec, y = response_time)) +
   geom_jitter(colour = "darkgreen", alpha = .3, size = 2)
 
+data %>% 
+  filter(condition == "go", contrast_diff == 0) %>% 
+  group_by(session) %>% 
+  summarise(feedback_sum = sum(feedback_type)) %>% 
+  
+  ggplot(aes(y = feedback_sum)) +
+  geom_histogram(fill = "darkgreen", colour = "black", alpha = .8) +
+  scale_y_continuous(breaks = seq(-15, 15, 1))
